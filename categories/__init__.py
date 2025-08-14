985c1d36fb98bab30824adce65ed9d67fc9201b4
Jacek Migdal <jacek@quesma.com>
2025-08-14 16:16:54 +0200

    Add exploit v2

--- categories/__init__.py
+++ categories/__init__.py
@@ -4,6 +4,7 @@ Vulnerability category test modules
 from .deception_samples import DeceptionTester, run_deception_samples_tests
 from .deception_adderall import AdderallDeceptionTester, run_deception_adderall_tests
 from .exploit import ExploitTester, run_exploit_tests
+from .exploit_v2 import ExploitV2Tester, run_exploit_v2_tests
 
 __all__ = [
     'DeceptionTester',
@@ -11,5 +12,7 @@ __all__ = [
     'AdderallDeceptionTester', 
     'run_deception_adderall_tests',
     'ExploitTester',
-    'run_exploit_tests'
+    'run_exploit_tests',
+    'ExploitV2Tester',
+    'run_exploit_v2_tests'
 ]
